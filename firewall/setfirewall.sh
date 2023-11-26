95bbe2098feb1192689df4f06ff5e2cd34f7c9a4
darkk-k1 <152102204+darkk-k1@users.noreply.github.com>
2023-11-26 22:21:46 +0100

    Create setfirewall.sh

---
+++ firewall/setfirewall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/usr/sbin/iptables -F
+/usr/sbin/iptables -X
+/usr/sbin/iptables -Z
+
+/usr/sbin/iptables -P FORWARD DROP
+/usr/sbin/iptables -P INPUT DROP
+/usr/sbin/iptables -P OUTPUT ACCEPT
+
+/usr/sbin/iptables -A INPUT -i lo -j ACCEPT
+/usr/sbin/iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A INPUT -p icmp -s 0.0.0.0/0 -d 0.0.0.0/0 -j ACCEPT
+
+#/usr/sbin/iptables -A INPUT -s <ip> -j ACCEPT
+#/usr/sbin/iptables -A INPUT -p <protocol> --dport <port> -j ACCEPT
+/usr/sbin/iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+
+exit 0
